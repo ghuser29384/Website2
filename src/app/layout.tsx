35f4b2029427d01121497863bc84f7ae07f21abb
ghuser29384 <262476329+ghuser29384@users.noreply.github.com>
2026-07-16 16:40:24 +0800

    Layer the editorial palette after search polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import "./readability-cleanup.css";
 import "./activation-critical.css";
 import "./home-process-polish.css";
 import "./home-mode-hover-colors.css";
+import "./search-bar-polish.css";
 import "./editorial-colorway.css";
 
 export const metadata: Metadata = {
